07ac776183d11d579d4fc0d2e0bb37f1b4ca927b
Kamil Ratowski <kamilratowski90@gmail.com>
2024-08-05 14:05:49 +0200

    Created scenario when exception is thrown and caught

--- src/com/moshjavaexercises/exceptions/ExceptionsDemo.java
+++ src/com/moshjavaexercises/exceptions/ExceptionsDemo.java
@@ -1,7 +1,16 @@
 package com.moshjavaexercises.exceptions;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExceptionsDemo {
     public static void show() {
-
+        try (var reader = new FileReader("file.txt")) {
+            System.out.println("Reading file");
+            var value = reader.read();
+            System.out.println(value);
+        } catch (IOException e) {
+            System.out.println("Failed to read the file");
+        }
     }
 }
