3ba472569de1774aec0bf81bef679105986f1f74
Kamil Ratowski <kamilratowski90@gmail.com>
2024-08-05 14:08:40 +0200

    Added multiple exception handling

--- src/com/moshjavaexercises/exceptions/ExceptionsDemo.java
+++ src/com/moshjavaexercises/exceptions/ExceptionsDemo.java
@@ -2,6 +2,8 @@ package com.moshjavaexercises.exceptions;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
@@ -9,8 +11,11 @@ public class ExceptionsDemo {
             System.out.println("Reading file");
             var value = reader.read();
             System.out.println(value);
+            new SimpleDateFormat().parse("");
         } catch (IOException e) {
             System.out.println("Failed to read the file");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
     }
 }
